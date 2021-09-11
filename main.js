bcb78dd331392a883b7ac8c837e0f929297da4e8
Dmytro <dimaniko04@ukr.net>
2021-09-11 08:06:02 +0300

    Used another approach

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const [union, intersection, difference, complement] = require("./functions.js");
 const readData = require("./readData.js");
 
-const SETS_LIMIT = 4;
+const SETS_LIMIT = 3;
 
 const sets = {
   A: 0,
@@ -10,95 +10,98 @@ const sets = {
 };
 
 const operations = {
-  I: intersection,
-  U: union,
-  "-": difference,
+  n: { func: complement, priority: 4 },
+  I: { func: intersection, priority: 3 },
+  U: { func: union, priority: 2 },
+  "\u005C": { func: difference, priority: 1 },
 };
 
-function doTheOperations(formula) {
-  Object.entries(operations).forEach(([key, operation]) => {
-    while (true) {
-      if (formula.indexOf(key) === -1) {
-        break;
-      }
+function doTheOperations(allSets, parsedFormula) {
+  const stack = [];
 
-      const operationIndex = formula.indexOf(key);
-      const firstSet = formula[operationIndex - 1];
-      const secondSet = formula[operationIndex + 1];
-      const newSet = operation(firstSet, secondSet);
+  parsedFormula.forEach((element) => {
+    if (element === "n") {
+      const last = stack.pop();
+      const universal = allSets[SETS_LIMIT - 1];
 
-      formula.splice(operationIndex - 1, 3, newSet);
+      stack.push(complement(last, universal));
+    } else if (operations[element]) {
+      const y = stack.pop();
+      const x = stack.pop();
+
+      stack.push(operations[element].func(x, y));
+    } else {
+      const setNumber = sets[element];
+
+      stack.push(allSets[setNumber]);
     }
   });
 
-  const result = formula[0];
-  return result;
+  return stack[0];
 }
+
+const polishNotation = (formula) => {
+  const result = [];
+  const stack = [];
+
+  for (const element of formula) {
+    if (operations[element]) {
+      while (stack.length) {
+        const last = stack[stack.length - 1];
+
+        if (
+          last == "(" ||
+          operations[element].priority >= operations[last].priority
+        ) {
+          break;
+        }
+
+        result.push(stack.pop());
+      }
+
+      stack.push(element);
+    }
+    if (element === "(") {
+      stack.push(element);
+    }
+    if (element === ")") {
+      while (stack.length) {
+        const last = stack.pop();
+
+        if (last === "(") {
+          break;
+        }
+
+        result.push(last);
+      }
+    }
+    if (sets[element] != undefined) {
+      result.push(element);
+    }
+  }
+
+  while (stack.length) {
+    result.push(stack.pop());
+  }
+
+  return result;
+};
 
 function launchCalculations(allSets, formulaWithSpaces) {
   const formula = formulaWithSpaces.replace(/ +/g, "");
 
-  let index = -1;
-
-  const parseFormula = () => {
-    const parsedFormula = [];
-
-    while (true) {
-      index = index + 1;
-
-      if (index >= formula.length || formula[index] === ")") {
-        return parsedFormula;
-      }
-
-      const element = formula[index];
-
-      if (sets[element] !== undefined) {
-        const setNumber = sets[element];
-
-        parsedFormula.push(allSets[setNumber]);
-      }
-      if (operations[element]) {
-        parsedFormula.push(element);
-      }
-      if (element === "(") {
-        const parsedBrackets = parseFormula();
-        const resultInBrackets = doTheOperations(parsedBrackets);
-
-        parsedFormula.push(resultInBrackets);
-      }
-      if (element === "n") {
-        index = index + 1;
-
-        const parsedBrackets = parseFormula();
-        const resultInBrackets = doTheOperations(parsedBrackets);
-        const complementResult = complement(
-          resultInBrackets,
-          allSets[SETS_LIMIT - 1]
-        );
-
-        parsedFormula.push(complementResult);
-      }
-    }
-  };
-
-  const parsedFormula = parseFormula();
-  const result = doTheOperations(parsedFormula);
+  const parsedFormula = polishNotation(formula);
+  const result = doTheOperations(allSets, parsedFormula);
 
   return result;
 }
 
 function main(inputs, formula) {
-  const allSets = inputs.map((set) =>
-    set.trim().replace(/ +/g, " ").split(" ")
-  );
-
-  console.log("\nYour sets:");
-  allSets.forEach((set) => console.log(`{${set}}`));
-  console.log("");
+  const allSets = [...inputs];
 
   const result = launchCalculations(allSets, formula);
 
-  console.log("RESULT:");
+  console.log("\nRESULT:");
   console.log(`{${result}}`);
 }
 
--- readData.js
+++ readData.js
@@ -1,30 +1,56 @@
 const readline = require("readline");
+const [union] = require("./functions.js");
 
 const rl = readline.createInterface(process.stdin);
 
-const SETS_LIMIT = 4;
+const SETS_LIMIT = 3;
 
-const enumeration = {
-  1: "first",
-  2: "second",
-  3: "third",
-  4: "universal ",
+const setRequest = (setName) => `Type in elements of the set ${setName}:`;
+
+const logs = {
+  0: setRequest("A"),
+  1: setRequest("B"),
+  2: setRequest("C"),
+  3: `Enter the set names and the actions you want to perform to them:
+  U - union
+  I - intersection
+  \u005C - difference
+  n - complement 
+  () - brackets`,
+};
+
+const normalize = (line) => {
+  return line.trim().replace(/ +/g, " ").split(" ");
 };
 
 async function readData(callback) {
   const inputs = [];
 
+  console.log(logs[0]);
   for await (const line of rl) {
-    const index = inputs.length + 1;
+    const index = inputs.length;
 
-    if (index <= SETS_LIMIT) {
-      inputs.push(line);
+    if (index < SETS_LIMIT - 1) {
+      inputs.push(normalize(line));
+    } else if (index === SETS_LIMIT - 1) {
+      inputs.push(normalize(line));
+      const universalSet = inputs.reduce((accumulator = [], input) =>
+        union(accumulator, input)
+      );
+
+      console.log("\nYour sets:");
+      inputs.forEach((input) => console.log(`{${input}}`));
+      console.log("Universal set:");
+      console.log(`{${universalSet}}`);
     } else {
       const formula = line;
 
-      callback(inputs, formula);
       rl.close();
+      callback(inputs, formula);
+      return;
     }
+
+    console.log(logs[inputs.length]);
   }
 }
 
